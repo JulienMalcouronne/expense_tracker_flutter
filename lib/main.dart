2499d8f3b0e8fdfe28f3c53cbcce2a96ea5bdef8
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-26 17:22:33 +0100

    added a chartbar that show total expenses per day

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:expense_tracker_flutter/widgets/chart.dart';
 import 'package:expense_tracker_flutter/widgets/transaction_list.dart';
 
 import './widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
-
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
+import './widgets/chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -60,6 +61,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String title, double amount) {
     final newTransaction = Transaction(
       id: DateTime.now().toString(),
@@ -102,14 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
