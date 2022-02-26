93e623fe7231915aa41d2527df8bd0b3263eb586
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-26 18:21:31 +0100

    added a datapicker from flutter to allow user to add the date of its expense

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,13 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.purple,
           accentColor: Colors.amber,
           textTheme: ThemeData.light().textTheme.copyWith(
-                  headline6: TextStyle(
-                fontFamily: 'OpenSans',
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              )),
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+                button: TextStyle(color: Colors.white),
+              ),
           appBarTheme: AppBarTheme(
             textTheme: ThemeData.light().textTheme.copyWith(
                   headline6: TextStyle(
